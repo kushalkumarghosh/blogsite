6999930535ae914e18dc8ddadfe9a3ecae663c51
kushalkumarghosh <cse.kushal1@gmail.com>
2024-01-19 15:34:24 +0600

    Navbar section addded

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Blogs from './pages/Blogs.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs/>
+      },
+      {
+        path: "/about",
+        element: <About/>
+      },
+      {
+        path: "/Contact",
+        element: <Contact/>
+      },
+      {
+        path: "/services",
+        element: <Services/>
+      },
+    ]
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
