b3e124b93f06d1c4e384dee4b87b245d82e8ed11
kushalkumarghosh <cse.kushal1@gmail.com>
2024-01-20 14:25:34 +0600

    added menu items for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,89 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 //react icons
-import { FaSquareFacebook, FaSquareDribbble, FaTwitter, FaBars  } from "react-icons/fa6";
-
+import {
+  FaSquareFacebook,
+  FaSquareDribbble,
+  FaTwitter,
+  FaBars,
+  FaXmark,
+} from "react-icons/fa6";
 
 const Navbar = () => {
-    //navItems
-    const navItems = [
-        {path: "/", link: "Home"},
-        {path: "/services", link: "Services"},
-        {path: "/about", link: "About"},
-        {path: "/blogs", link: "Blogs"},
-        {path: "/contact", link: "Contact"},
-    ]
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  //navItems
+  const navItems = [
+    { path: "/", link: "Home" },
+    { path: "/services", link: "Services" },
+    { path: "/about", link: "About" },
+    { path: "/blogs", link: "Blogs" },
+    { path: "/contact", link: "Contact" },
+  ];
   return (
-    <header className='bg-black text-white fixed top-0 left-0 right-0'>
-        <nav className='px-4 py-4 max-w-7xl mx-auto flex justify-between items-center'>
-            <a href="/" className='text-xl font-bold text-white'>Design<span className='text-orange-500'>DK</span></a>
+    <header className="bg-black text-white fixed top-0 left-0 right-0">
+      <nav className="px-4 py-4 max-w-7xl mx-auto flex justify-between items-center">
+        <a href="/" className="text-xl font-bold text-white">
+          Design<span className="text-orange-500">DK</span>
+        </a>
 
-            {/* navItems for lg devices */}
-            <ul className='md:flex gap-12 text-lg hidden'>
-                {
-                    navItems.map(({path, link}) => <li className='text-white' key={path}>
-                        <NavLink to={path}>{link}</NavLink>
-                    </li>)
-                }
-            </ul>
+        {/* navItems for lg devices */}
+        <ul className="md:flex gap-12 text-lg hidden">
+          {navItems.map(({ path, link }) => (
+            <li className="text-white" key={path}>
+              <NavLink to={path}>{link}</NavLink>
+            </li>
+          ))}
+        </ul>
 
-            {/* menu icons */}
-            <div className='text-white lg:flex gap-4 items-center hidden'>
-                <a href="/" className='hover:text-orange-500'><FaSquareFacebook /></a>
-                <a href="/" className='hover:text-orange-500'><FaSquareDribbble /></a>
-                <a href="/" className='hover:text-orange-500'><FaTwitter /></a>
-                <button className='bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white
-                hover:text-orange-500 transition-all duration-200 ease-in'>Log in</button>
-            </div>
+        {/* menu icons */}
+        <div className="text-white lg:flex gap-4 items-center hidden">
+          <a href="/" className="hover:text-orange-500">
+            <FaSquareFacebook />
+          </a>
+          <a href="/" className="hover:text-orange-500">
+            <FaSquareDribbble />
+          </a>
+          <a href="/" className="hover:text-orange-500">
+            <FaTwitter />
+          </a>
+          <button
+            className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white
+                hover:text-orange-500 transition-all duration-200 ease-in"
+          >
+            Log in
+          </button>
+        </div>
 
-            {/* mobile menu btn, display mobile screen */}
-            <div className='md:hidden'>
-                <button className='cursor-pointer'><FaBars className='w-5 h-5'/></button>
-            </div>
+        {/* mobile menu btn, display mobile screen */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="cursor-pointer">
+            {isMenuOpen ? (
+              <FaXmark className="w-5 h-5" />
+            ) : (
+              <FaBars className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+      </nav>
 
-        </nav>
+      {/* menu items only for mobile */}
+
+      <div>
+        <ul className="md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white">
+          {navItems.map(({ path, link }) => (
+            <li className="text-black" key={path}>
+              <NavLink to={path}>{link}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
