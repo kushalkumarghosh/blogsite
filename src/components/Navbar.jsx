9e25da9a699eeac9b04f23deaf8bd5f98663e6d7
kushalkumarghosh <cse.kushal1@gmail.com>
2024-01-20 15:46:58 +0600

    finished the responsive navabr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,14 @@ const Navbar = () => {
         <ul className="md:flex gap-12 text-lg hidden">
           {navItems.map(({ path, link }) => (
             <li className="text-white" key={path}>
-              <NavLink to={path}>{link}</NavLink>
+              <NavLink className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+               to={path}>{link}</NavLink>
             </li>
           ))}
         </ul>
@@ -74,10 +81,16 @@ const Navbar = () => {
       {/* menu items only for mobile */}
 
       <div>
-        <ul className="md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white">
+        <ul
+          className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${
+            isMenuOpen
+              ? "fixed top-0 left-0 w-full transition-all ease-out duration-150"
+              : "hidden"
+          }`}
+        >
           {navItems.map(({ path, link }) => (
             <li className="text-black" key={path}>
-              <NavLink to={path}>{link}</NavLink>
+              <NavLink onClick={toggleMenu} to={path}>{link}</NavLink>
             </li>
           ))}
         </ul>
