80164925411a91fccc8f95b5c7e61bacd16034f0
kushalkumarghosh <cse.kushal1@gmail.com>
2024-01-22 22:04:27 +0600

    added the sidebar

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,64 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import BlogCards from './BlogCards';
-import Pagination from './Pagination';
-import CategorySelection from './CategorySelection';
+import React, { useEffect, useState } from "react";
+import BlogCards from "./BlogCards";
+import Pagination from "./Pagination";
+import CategorySelection from "./CategorySelection";
+import SideBar from "./SideBar";
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 12 //blogs per page
+  const pageSize = 12; //blogs per page
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [activeCategory, setActiveCategory] = useState(null);
 
-  useEffect(()=>{
-    async function fetchBlogs(){
+  useEffect(() => {
+    async function fetchBlogs() {
       let url = `http://localhost:5173/blogsData.json?page=${currentPage}&limit=${pageSize}`;
 
       //filter by category
-      if(selectedCategory){
+      if (selectedCategory) {
         url += `&category = ${selectedCategory}`;
       }
       const response = await fetch(url);
       const data = await response.json();
-     
+
       setBlogs(data);
     }
     fetchBlogs();
-  },[currentPage, pageSize, selectedCategory])
-  
+  }, [currentPage, pageSize, selectedCategory]);
 
   //page changing btn
-  const handlePageChange = (pageNumber) =>{
+  const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
-  }
+  };
 
-  const handleCategoryChange = (category) =>{
+  const handleCategoryChange = (category) => {
     setSelectedCategory(category);
     setCurrentPage(1);
     setActiveCategory(category);
-  }
+  };
 
   return (
     <div>
       {/* category section */}
       <div>
-        <CategorySelection onSelectCategory = {handleCategoryChange} selectedCategory = {selectedCategory}
-        activeCategory = {activeCategory}/>
+        <CategorySelection
+          onSelectCategory={handleCategoryChange}
+          selectedCategory={selectedCategory}
+          activeCategory={activeCategory}
+        />
       </div>
 
       {/* blogCards Section */}
-      <div>
-        <BlogCards blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pageSize=
-        {pageSize}/>
+      <div className="flex flex-col lg:flex-row gap-12">
+        {/* blog cards component */}
+        <BlogCards
+          blogs={blogs}
+          currentPage={currentPage}
+          selectedCategory={selectedCategory}
+          pageSize={pageSize}
+        />
+
+        {/* SideBar component */}
+        <div>
+          <SideBar />
+        </div>
       </div>
 
       {/* pagination section */}
       <div>
-        <Pagination onPageChange={handlePageChange} currentPage = {currentPage} blogs = {blogs} pageSize=
-        {pageSize}/>
+        <Pagination
+          onPageChange={handlePageChange}
+          currentPage={currentPage}
+          blogs={blogs}
+          pageSize={pageSize}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
